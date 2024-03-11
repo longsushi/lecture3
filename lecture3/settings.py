56f01ed0956cd6a4e717237ad7363e386b5cc124
Elias <elias.haghverdi@gmail.com>
2024-03-11 23:42:13 +0100

    some changes

--- lecture3/settings.py
+++ lecture3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j3r#br0rqavstzm8hp#-1r!97_)tq7=n=rbrb$fv-bjr==al3g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
