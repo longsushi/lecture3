ed28dc9f5741fbba5e23b00b3f5dd23c3727f838
Elias <elias.haghverdi@gmail.com>
2024-03-13 14:32:51 +0100

    started task list and created table

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+tasks = [("Müll raus", 1), ("trinken", 2), ("essen", 3)]
+
+def index(request):
+    return render(request, "tasks/index.html", {"tasks":tasks})
+
+def add(request):
+    return render(reqeust, "tasks/add.html")
