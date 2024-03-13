0f11bb81b12caa1d207311008dddaa7f1dbf7dc0
Elias <elias.haghverdi@gmail.com>
2024-03-13 01:33:48 +0100

    test

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "hello/index.html")
+    #test
 
 def greet(request, name):
     return render(request, "hello/greet.html", {"name":name.capitalize()})
